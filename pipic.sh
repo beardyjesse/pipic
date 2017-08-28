56ffe2752130bef8237b1939e38db4d78fe0905b
beardyjesse <andrew.jesse1@gmail.com>
2017-08-28 14:41:19 +0100

    added comments and improvements
    tidied the variables to the top and made the folder a variable
    added comments throughout
    added delay as a variable so that it could be changed in the variable section and now writes that to a file in the folder for historical reference
    changed resolution to V2 of the

--- pipic.sh
+++ pipic.sh
@@ -1,12 +1,40 @@
 #! /bin/bash
-read -r FOLDER</home/pi/number
-mkdir -p /home/pi/$FOLDER
-echo $[$FOLDER+1]>/home/pi/number
+
+### SET VARIABLES AND FAFF AT START ####
+
+# define wait period between images and set incremental number for loop
+DELAY=15
 NUM=0
+ROOT_FOLDER=/home/pi
+
+# read in the next folder number, make that folder and increment folder number for next reboot
+read -r FOLDER<$ROOT_FOLDER/number
+mkdir -p $ROOT_FOLDER/$FOLDER
+echo $[$FOLDER+1]>$ROOT_FOLDER/number
+
+# record the delay between photos
+echo "$DELAY seconds" > $ROOT_FOLDER/$FOLDER/delay.var
+
+
+
+### TIMELAPSE SCRIPT ###
+
+# infinite loop which takes photos
 while true
 do
+
+# make incremental number 5 digid with leading zeros
 BER="$(printf "%05d" $NUM)"
-raspistill -w 2592 -h 1458 -vf -hf --nopreview -o /home/pi/$FOLDER/"$FOLDER"_oggcamp2017_$BER.jpg
+
+# take photo, increment loop number and wait
+raspistill -w 3280 -h 1845 -vf -hf --nopreview -o $ROOT_FOLDER/$FOLDER/"$FOLDER"_loungeDIY_$BER.jpg
 NUM=$[$NUM+1]
-sleep 10
+sleep $DELAY
+
+# check to see if it is time to stop taking photos for backup & reset
+if [ `date +%H` -eq 23 ]; then
+break
+fi
+
 done
+
